572aa6730073af6f70d42564bbd1239cf79e0f1d
Brad Newman <newmanbradm@gmail.com>
2018-05-17 19:07:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    last_item = ", and " << array.pop
+    array.join(", ") << last_item
+  end
 end
