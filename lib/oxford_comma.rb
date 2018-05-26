ebab28d8296222fceb78adc2911b661fbff66c85
Changmin Lim <chang.lim92@gmail.com>
2018-05-26 04:31:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length 
+  when 1 
+    array.join
+  when 2 
+    array.join(" and ")
+  else 
+    array.push("and " + array.pop)
+    array.join(", ")
+  end 
 end
