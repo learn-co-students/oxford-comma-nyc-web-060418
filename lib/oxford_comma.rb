7e019cad474a7ba605847bf4f9ea4293434c7533
slowbeam <sedwardscode@gmail.com>
2018-05-27 19:32:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+  i = array[-1]
+  array.pop
+  list = array.join(", ")
+  list << ", and #{i}"
+  end
 end
