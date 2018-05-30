a5b964aa9c8b03fa83a94a9df2f3623f42cf2783
noah-berman <noahberm@gmail.com>
2018-05-30 17:54:23 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    string = ""
+    until array.length == 2
+      string << "#{array.shift}, "
+    end
+    string << "#{array.shift}, and "
+    string << array.shift
+    string
+  end
 end
